d49f847784f066d94a171f15641c8e81118cb3cb
Kaique <kaiquesc2013@uni9.edu.br>
2024-09-29 18:57:55 -0300

    First Commit

---
+++ GrandDevOpsSanAndreas/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace GrandDevOpsSanAndreas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            lblEnergy = new Label();
+            lblVest = new Label();
+            lblStarsZero = new Label();
+            lblStar = new Label();
+            lblCash = new Label();
+            lblHour = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Image = (Image)resources.GetObject("label1.Image");
+            label1.Location = new Point(752, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(222, 79);
+            label1.TabIndex = 0;
+            // 
+            // lblEnergy
+            // 
+            lblEnergy.Image = (Image)resources.GetObject("lblEnergy.Image");
+            lblEnergy.Location = new Point(858, 105);
+            lblEnergy.Name = "lblEnergy";
+            lblEnergy.Size = new Size(114, 8);
+            lblEnergy.TabIndex = 1;
+            // 
+            // lblVest
+            // 
+            lblVest.Image = (Image)resources.GetObject("lblVest.Image");
+            lblVest.Location = new Point(858, 82);
+            lblVest.Name = "lblVest";
+            lblVest.Size = new Size(114, 8);
+            lblVest.TabIndex = 2;
+            // 
+            // lblStarsZero
+            // 
+            lblStarsZero.Image = (Image)resources.GetObject("lblStarsZero.Image");
+            lblStarsZero.Location = new Point(761, 158);
+            lblStarsZero.Name = "lblStarsZero";
+            lblStarsZero.Size = new Size(212, 38);
+            lblStarsZero.TabIndex = 3;
+            // 
+            // lblStar
+            // 
+            lblStar.Image = (Image)resources.GetObject("lblStar.Image");
+            lblStar.Location = new Point(934, 158);
+            lblStar.Name = "lblStar";
+            lblStar.Size = new Size(39, 38);
+            lblStar.TabIndex = 4;
+            // 
+            // lblCash
+            // 
+            lblCash.AutoSize = true;
+            lblCash.BackColor = Color.Transparent;
+            lblCash.Font = new Font("Pricedown Bl", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblCash.Location = new Point(752, 115);
+            lblCash.Name = "lblCash";
+            lblCash.Size = new Size(234, 42);
+            lblCash.TabIndex = 5;
+            lblCash.Text = "R$0000000000";
+            // 
+            // lblHour
+            // 
+            lblHour.AutoSize = true;
+            lblHour.BackColor = Color.Transparent;
+            lblHour.Font = new Font("Pricedown Bl", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblHour.Location = new Point(861, 20);
+            lblHour.Name = "lblHour";
+            lblHour.Size = new Size(113, 58);
+            lblHour.TabIndex = 6;
+            lblHour.Text = "10:10";
+            // 
+            // label3
+            // 
+            label3.BackColor = SystemColors.Control;
+            label3.Font = new Font("Times New Roman", 11.25F);
+            label3.Location = new Point(77, 232);
+            label3.Name = "label3";
+            label3.Size = new Size(409, 151);
+            label3.TabIndex = 8;
+            label3.Text = "Alternativa 1";
+            // 
+            // label2
+            // 
+            label2.BackColor = SystemColors.Control;
+            label2.Font = new Font("Times New Roman", 11.25F);
+            label2.Location = new Point(565, 232);
+            label2.Name = "label2";
+            label2.Size = new Size(409, 151);
+            label2.TabIndex = 9;
+            label2.Text = "Alternativa 2";
+            // 
+            // label4
+            // 
+            label4.BackColor = SystemColors.Control;
+            label4.Font = new Font("Times New Roman", 11.25F);
+            label4.Location = new Point(77, 423);
+            label4.Name = "label4";
+            label4.Size = new Size(409, 166);
+            label4.TabIndex = 10;
+            label4.Text = "Alternativa 3";
+            // 
+            // label5
+            // 
+            label5.BackColor = SystemColors.Control;
+            label5.Font = new Font("Times New Roman", 11.25F);
+            label5.Location = new Point(565, 423);
+            label5.Name = "label5";
+            label5.Size = new Size(409, 166);
+            label5.TabIndex = 11;
+            label5.Text = "Alternativa 4";
+            // 
+            // label6
+            // 
+            label6.BackColor = SystemColors.Control;
+            label6.Font = new Font("Arial Narrow", 12F);
+            label6.Location = new Point(42, 82);
+            label6.Name = "label6";
+            label6.Size = new Size(681, 120);
+            label6.TabIndex = 12;
+            label6.Text = "PERGUNTA";
+            // 
+            // label7
+            // 
+            label7.BackColor = SystemColors.Control;
+            label7.Font = new Font("Arial Black", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(42, 36);
+            label7.Name = "label7";
+            label7.Size = new Size(681, 42);
+            label7.TabIndex = 13;
+            label7.Text = "QUESTÃO - 1";
+            // 
+            // label8
+            // 
+            label8.Image = (Image)resources.GetObject("label8.Image");
+            label8.Location = new Point(29, 217);
+            label8.Name = "label8";
+            label8.Size = new Size(42, 42);
+            label8.TabIndex = 14;
+            // 
+            // label9
+            // 
+            label9.Image = (Image)resources.GetObject("label9.Image");
+            label9.Location = new Point(517, 217);
+            label9.Name = "label9";
+            label9.Size = new Size(42, 42);
+            label9.TabIndex = 15;
+            // 
+            // label10
+            // 
+            label10.Image = (Image)resources.GetObject("label10.Image");
+            label10.Location = new Point(517, 409);
+            label10.Name = "label10";
+            label10.Size = new Size(42, 42);
+            label10.TabIndex = 16;
+            // 
+            // label11
+            // 
+            label11.Image = (Image)resources.GetObject("label11.Image");
+            label11.Location = new Point(29, 409);
+            label11.Name = "label11";
+            label11.Size = new Size(42, 42);
+            label11.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1015, 633);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(lblHour);
+            Controls.Add(lblStar);
+            Controls.Add(lblCash);
+            Controls.Add(lblVest);
+            Controls.Add(lblEnergy);
+            Controls.Add(lblStarsZero);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Grand DevOps San Andreas v1.0 | ByteGenius Group";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label lblEnergy;
+        private Label lblVest;
+        private Label lblStarsZero;
+        private Label lblStar;
+        private Label lblCash;
+        private Label lblHour;
+        private Label label3;
+        private Label label2;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+    }
+}
